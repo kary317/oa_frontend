e688f46629d622f81d4f5ebb6a0b7038166ed7bf
kary <kary.sun@outlook.com>
2025-12-16 16:25:14 +0800

    首页最新通知和最新考勤功能实现

--- src/api/homeHttp.js
+++ src/api/homeHttp.js
@@ -5,6 +5,18 @@ const getDepartmentStaffCount = () => {
   return http.get(path);
 };
 
+const getLatestInforms = () => {
+  const path = "/home/latest/inform/";
+  return http.get(path);
+};
+
+const getLatestAbsents = () => {
+  const path = "/home/latest/absent/";
+  return http.get(path);
+};
+
 export default {
   getDepartmentStaffCount,
+  getLatestInforms,
+  getLatestAbsents,
 };
